fe120fae508a55ec1d516139585d1bcf2a88ae9a
EDWINCSN <c.edwin32@hotmail.com>
2026-09-22 01:08:40 -0500

    feat: configura App.tsx con Bottom Tab Navigator

--- App.tsx
+++ App.tsx
@@ -1,20 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { ProductoProvider } from './src/context/ProductoContext';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import NuevoProductoScreen from './src/screens/NuevoProductoScreen';
+import ProductosScreen from './src/screens/ProductosScreen';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+// DICCIONARIO
+export type RootTabParamList = {
+  ListaProductos: undefined;
+  AddProductos: undefined;
+}
+
+const Tab = createBottomTabNavigator<RootTabParamList>();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ProductoProvider>
+      <SafeAreaProvider>
+      <NavigationContainer>
+        <Tab.Navigator initialRouteName='ListaProductos'>
+          <Tab.Screen
+            name='ListaProductos'
+            component={ProductosScreen}
+            options={{ title: 'Lista Productos' }}
+          />
+
+          <Tab.Screen
+            name='AddProductos'
+            component={NuevoProductoScreen}
+            options={{ title: 'Nuevo Producto' }}
+          />
+        </Tab.Navigator>
+      </NavigationContainer>
+      </SafeAreaProvider>
+    </ProductoProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/screens/ProductosScreen.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+export default function ProductosScreen() {
+  return (
+    <SafeAreaView>
+      <Text>ProductosScreen ProductosScreen</Text>
+    </SafeAreaView>
+  );
+}
